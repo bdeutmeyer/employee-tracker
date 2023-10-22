129ff5ac8a90ba55d7512b83ba55f022c072fbde
beth deutmeyer <bethdeutmeyer@gmail.com>
2023-10-22 12:15:40 -0500

    connected db to node, began functional logic

---
+++ prompts/initialPrompts.js
@@ -0,0 +1,9 @@
+const initialPrompts = {
+    type: 'list',
+    name: 'options',
+    message: 'What would you like to do?',
+    choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit'],
+    loop: true
+  };
+
+  module.exports = initialPrompts;
--- server.js
+++ server.js
@@ -1,28 +1,70 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const initialPrompts = require('./prompts/initialPrompts');
 
 // create the connection to database
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
     password: 'rootroot',
-    database: 'test'
-  });
-  
-  // simple query
-  connection.query(
-    'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
-    function(err, results, fields) {
-      console.log(results); // results contains rows returned by server
-      console.log(fields); // fields contains extra meta data about results, if available
-    }
+    database: 'employee_data_db'
+  },
+  console.log('Connected to the database'),
   );
-  
-  // with placeholder
-  connection.query(
-    'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
-    ['Page', 45],
-    function(err, results) {
-      console.log(results);
+
+
+const init = () => {
+  inquirer  
+  .prompt(initialPrompts)
+  .then((answers) => {
+    switch (answers.options) {
+      case 'View all departments':
+        db.query('SELECT * FROM department', function (err, results) {
+          err ? console.log(err) : console.table(results);
+          init();
+        });
+        break;
+      case 'View all roles':
+        db.query('SELECT * FROM role', function (err, results) {
+          err ? console.log(err) : console.table(results);
+          init();
+        });
+        break;
+      case 'View all employees':
+        db.query('SELECT * FROM employee', function (err, results) {
+          err ? console.log(err) : console.table(results);
+          init();
+        });
+        break;
+      case 'Add a department':
+        //function
+
+        break;
+      case 'Add a role':
+        //function
+
+        break;
+      case 'Add an employee':
+        //function
+
+        break;   
+      case 'Update an employee role':
+        //function
+
+        break;
+      case 'Quit':
+        //function
+        break;       
+    } 
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
     }
-  );
+  })
+}
+
+module.exports = db;
+init();
