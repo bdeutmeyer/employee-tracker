2a78555ddd88bc58364c280a0f084f77a7b9d4bf
beth deutmeyer <bethdeutmeyer@gmail.com>
2023-10-24 11:29:47 -0500

    was working up to update, now not recognizing functions

--- prompts/addRolePrompts.js
+++ prompts/addRolePrompts.js
@@ -1,4 +1,25 @@
-const updatedDeptChoices = require('./updateArrayFunctions');
+const mysql = require('mysql2');
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'rootroot',
+    database: 'employee_data_db'
+  },
+);
+
+const updatedDeptChoices = [];
+const updateDepts = () => {
+    db.query('Select name from department ORDER BY department.id ASC', function (err, results) {
+        if (err) {
+            console.log(err)
+        } else {
+            for(let i = 0; i < results.length; i++) {
+                updatedDeptChoices.push(results[i].name);
+            };
+        }
+    })
+};
+
 const addRolePrompts = [
     {
         type: 'input',
@@ -18,4 +39,4 @@ const addRolePrompts = [
     }
 ];
 
-module.exports = addRolePrompts;
+module.exports = {updateDepts, addRolePrompts};
